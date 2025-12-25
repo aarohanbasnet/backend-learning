f8c51f1baea1531e24508e6625c07b5fdc506d86
aarohanbasnet <letsmailaarohan@gmail.com>
2025-12-25 22:53:04 +0545

    fixed bugs

--- dynamic-routing/index.js
+++ dynamic-routing/index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
-app.set("viewengine","ejs");
+app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req,res)=>{
-    res.render("index~")
+    res.render("index")
 })
 
 app.get("/home", function(req,res){
