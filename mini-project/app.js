dfe219ee567cbf0e7995e73cceac4b3e4b798bd3
aarohanbasnet <letsmailaarohan@gmail.com>
2025-12-27 22:24:43 +0545

    CRUD project initial setup

---
+++ mini-project/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.set('view engine',"ejs");
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req,res)=>{
+    res.render('index');
+});
+
+app.listen(3000, ()=>{
+    console.log('server is running');
+});
