4b4634f201e3d65b891d985c3f5d22eed17f9bd3
aarohanbasnet <letsmailaarohan@gmail.com>
2025-12-28 22:21:38 +0545

    Added Create and Delete Operation

--- mini-project/app.js
+++ mini-project/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+const userModel = require('./models/user');
+
 app.set('view engine',"ejs");
+
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,6 +13,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", (req,res)=>{
     res.render('index');
 });
+
+app.get("/read",async (req,res)=>{
+    let users = await userModel.find()
+    res.render("read", {users});
+})
+
+app.post("/create",async (req,res)=>{
+    let {name,email, image} = req.body;
+    let createdUser = await userModel.create({
+       // name : req.body.name
+       name, // name : name,
+       email,
+       image,
+    });
+
+    res.redirect("/read");
+});
+
+app.get("/delete/:id", async (req,res)=>{
+    let users = await userModel.findOneAndDelete({_id : req.params.id});
+    res.redirect("/read");
+})
 
 app.listen(3000, ()=>{
     console.log('server is running');
