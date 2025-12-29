3b5be432b0c8f3a6ae2da7fd505eaca175b055e3
aarohanbasnet <letsmailaarohan@gmail.com>
2025-12-29 19:10:28 +0545

    Added Update Feature

--- mini-project/app.js
+++ mini-project/app.js
@@ -36,6 +36,19 @@ app.get("/delete/:id", async (req,res)=>{
     res.redirect("/read");
 })
 
+//UPDATE
+
+app.get("/edit/:userid", async (req,res)=>{
+    let user = await userModel.findOne({_id : req.params.userid});
+    res.render("edit", {user});
+});
+
+app.post("/update/:userid", async (req,res)=>{
+    let {name, email, image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id : req.params.userid}, {image, name, email}, {new : true});
+    res.redirect("/read");
+})
+
 app.listen(3000, ()=>{
     console.log('server is running');
 });
