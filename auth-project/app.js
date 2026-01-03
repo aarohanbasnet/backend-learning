824b7dd56ca0428f2384ad762b489d2f7507f87c
aarohanbasnet <letsmailaarohan@gmail.com>
2026-01-03 23:21:12 +0545

    Implement bcrypt password hashing

--- auth-project/app.js
+++ auth-project/app.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const userModel = require('./models/usermodel');
+const bcrypt = require('bcrypt');
+const usermodel = require('./models/usermodel');
 
 const PORT = 3000;
 const app = express();
@@ -20,6 +22,26 @@ app.use(cookieParser());
 app.get("/", (req,res)=>{
     res.render("index");
 });
+
+app.post("/create", (req,res)=>{
+    let {username, email, age, password} = req.body;
+
+    bcrypt.genSalt(10, function(err,salt){
+        bcrypt.hash(password,salt, async (err, hash)=>{
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password : hash,
+                age
+            });
+
+        res.send(createdUser);
+        });
+    }) ;
+
+
+});
+
  
 app.listen(PORT, ()=>{
     console.log(`server is running in http://localhost:${PORT}`);
