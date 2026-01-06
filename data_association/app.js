5736ceffec5a6c7a818609a5cefb50d6896720f8
aarohanbasnet <letsmailaarohan@gmail.com>
2026-01-06 23:17:02 +0545

    Fix MongoDB connection port

--- data_association/app.js
+++ data_association/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userModel = require("./models/user");
 const postModel = require("./models/post");
+
 const app = express();
 
 
--- data_association/models/user.js
+++ data_association/models/user.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:2701//testingdb");
+mongoose.connect("mongodb://127.0.0.1:27017/testingdb");
 
 const userSchema = mongoose.Schema({
     username : String,
