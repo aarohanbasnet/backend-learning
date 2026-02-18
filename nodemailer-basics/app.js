c3c0533a14060e16bc082fc2a8be35b0b1e32e05
aarohanbasnet <letsmailaarohan@gmail.com>
2026-02-18 21:31:13 +0545

    feat : add email service with nodemailer

---
+++ nodemailer-basics/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = 5000;
+
+
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("Test passed");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running http://localhost:${PORT}`);
+})
+
+
---
+++ nodemailer-basics/serivces/emailService.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: "jody23@ethereal.email",
+    pass: "QspptC16CMXWvy6jnS",
+  },
+});
+
+const sendEmail = async function(targetEmail, subject, message){
+    try{
+        const info = transporter.sendMail({
+            from : '"Jody DuBuque" <intern@company.com>',
+            to : targetEmail,
+            subject : subject,
+            text : message,
+            html : `<b>${message}</b>`
+        });
+
+        console.log("Message sent : %s", (await info).messageId);
+        console.log("Preview URL : %s", nodemailer.getTestMessageUrl(info));
+
+        return info;
+
+    } catch(error){
+        console.error("Email failed to send :",error);
+        throw error
+    }
+}
+
+module.exports = {sendEmail};
