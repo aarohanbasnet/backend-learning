e89c8cea56480d0d7f05ac96ef83866d0574b57d
aarohanbasnet <letsmailaarohan@gmail.com>
2026-04-14 21:12:18 +0545

    feat(auth) : add access and refresh token generation utilities

--- advancedAuth/controllers/auth.controller.js
+++ advancedAuth/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const userModel = require("../models/user.model");
 
 const login = async(req, res)=>{
 try{
-    const {email, pasaword} = req.body;
+    const {email, password} = req.body;
     const user = await userModel.findOne({email});
     if(!user) return res.status(400).json({msg : "User not found"});
 } catch (error){
