53b8a19228852ea67100aa05502584db4a8031f5
aarohanbasnet <letsmailaarohan@gmail.com>
2026-04-14 21:18:00 +0545

    refactor (user) : update schema with validation rules, replacerefreshToken with isVerifed, enabled timestamps

--- advancedAuth/models/user.model.js
+++ advancedAuth/models/user.model.js
@@ -1,9 +1,9 @@
 const  mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email : String,
-    password : String,
-    refreshToken : String
-});
+    email : {type : String, required : true, uniquie : true},
+    password : {type : String, required : true},
+    isVerified : {type : Boolean, default : false}
+}, {timestamps : true});
 
 module.exports = mongoose.model("User", userSchema);
