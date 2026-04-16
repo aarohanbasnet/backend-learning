5a4b3d36f79ef5b0e676f92be3e4b471139a70ec
aarohanbasnet <letsmailaarohan@gmail.com>
2026-04-16 22:14:53 +0545

    feat ( auth ) : implement user login service

--- advancedAuth/services/auth.service.js
+++ advancedAuth/services/auth.service.js
@@ -1,5 +1,7 @@
 const { hashPassword } = require('../utils/hash');
 const User = require('../models/user.model');
+const {generateAccessToken, generateRefreshToken} = require('../utils/generateToken');
+
 
 const registerService = async ({email, password}) => {
     const existingUser = await User.findOne({email});
@@ -13,3 +15,19 @@ const registerService = async ({email, password}) => {
 
     return { msg : "User registered successfully"};
 }
+
+const loginService = async ({email, password}) => {
+    const user = await User.findOne({email});
+    if(!user) throw new Error("Invalid credentials");
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    await Token.create({
+        userId : user._id,
+        token : refreshToken,
+        expiresAt : new Date(Date.now()+7*24*60*60*1000)
+    });
+
+    return { accessToken, refreshToken };
+};
