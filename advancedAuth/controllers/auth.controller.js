78eabbccf40fe3b982c9d20cd164cd436f3e4d7c
aarohanbasnet <letsmailaarohan@gmail.com>
2026-04-18 22:46:24 +0545

    feat (auth) : Add register controller with registerService

--- advancedAuth/controllers/auth.controller.js
+++ advancedAuth/controllers/auth.controller.js
@@ -2,6 +2,18 @@ const bcrypt = require("bcrypt");
 const userModel = require("../models/user.model");
 const {loginService, registerService, refreshService} = require("../services/auth.service")
 
+const register = async(req, res)=>{
+    try{
+        const result = await registerService(req.body);
+    res.json(result);
+    } catch (err) {
+        res.status(400).json({
+            message : err.message
+        });
+    }
+    
+};
+
 const login = async(req, res)=>{
     try{
     const result = await loginService(req.body);
@@ -12,3 +24,4 @@ const login = async(req, res)=>{
         });
     }
 };
+
