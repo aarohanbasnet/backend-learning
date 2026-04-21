b24cbc6ad39063108c645e8fdfc1e6369de3242f
aarohanbasnet <letsmailaarohan@gmail.com>
2026-04-21 22:58:54 +0545

    feat : (auth) add protected route with verifyAccessToken middleware

--- advancedAuth/routes/auth.routes.js
+++ advancedAuth/routes/auth.routes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const {register, login, refresh} = require("../controllers/auth.controller");
+const verifyAccessToken = require("../middlewares");
 
 router.post("/login", login);
 router.post("/refresh", refresh);
+
+router.get("/me", verifyAccessToken, (req, res)=>{
+    res.json({msg : `Welcome ${req.user.id}` });
+});
 
 module.exports = router;
