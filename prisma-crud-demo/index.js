e5f3d2cff768f89ef89cc263b8cc4da11e54add4
aarohanbasnet <letsmailaarohan@gmail.com>
2026-07-05 22:50:12 +0545

    Initialized prisma and created Create Endpoint

---
+++ prisma-crud-demo/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client';
+
+const adapter = new PrismaPg({ connectionString : process.env.DATABASE_URL});
+const prisma = new PrismaClient({adapter});
+
+const app = express();
+app.use(express.json());
+
+app.post('/users', async( req, res)=>{
+    try{
+        const {name, email, age} = req.body;
+        const user = await prisma.user.create({
+            data : {name, email, age },
+        });
+         res.status(201).json({
+            success : true,
+            message : "User created successfully",
+            data : user,
+         });
+    } catch (err) {
+        res.status(400).json({
+            success : false, 
+            message : err.message,
+            data : null,
+        });
+    };
+});
+
+
+app.listen(3000, ()=>{
+    console.log("server running on port 3000");
+});
